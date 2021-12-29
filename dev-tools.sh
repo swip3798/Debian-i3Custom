622576b8f31761e99c47b4d9db582258bec74134
Christian <christian.sw@hotmail.de>
2021-12-29 14:01:57 +0100

    Add some dev tools install stuff

--- dev-tools.sh
+++ dev-tools.sh
@@ -1,29 +1,32 @@
 # This just installs the basic tools i need for most development tasks for my private software development
 mkdir -p ~/Documents/dev
 
+####### VSCODE #########
+mkdir vscode
+cd vscode
+wget "https://code.visualstudio.com/sha/download?build=stable&os=linux-deb-x64" -O vscode.deb
+sudo dpkg -i vscode.deb
+rm -f vscode.deb
+cd ..
+
+######## RUST ##########
 # install rust toolchain
 curl --proto '=https' --tlsv1.2 -sSf https://sh.rustup.rs | sh
 
-# Install vscode
-mkdir vscode
-cd vscode
-wget https://code.visualstudio.com/sha/download?build=stable&os=linux-deb-x64
-sudo dpkg -i *.deb
-cd ..
-
-# python stuff
+###### PYTHON ###########
 sudo apt install -y python3-pip
 pip install seaborn pandas mongoengine scikit-learn requests
 
 
-cd ~/Documents/dev
+####### FLUTTER #########
 # Install flutter (except android studio stuff, because of the pain) works for 2.8.1
 sudo snap install flutter --classic
 
+flutter config --no-analytics
 flutter doctor
 
 # Linux desktop dependencies
-sudo apt-get install clang cmake ninja-build pkg-config libgtk-3-dev
+sudo apt-get install -y clang cmake ninja-build pkg-config libgtk-3-dev
 # Enable desktop support
 flutter config --enable-linux-desktop
 
@@ -35,3 +38,4 @@ echo Press enter when you installed both android studio and command-line tools
 read -p " [RETURN]"
 flutter doctor --android-licenses
 flutter doctor
+
