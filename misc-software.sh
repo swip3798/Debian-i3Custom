06d4203c3ee64b3df956ccc041c47027f436560b
Christian <christian.sw@hotmail.de>
2021-12-29 22:52:48 +0100

    Fix wine deb stuff

--- misc-software.sh
+++ misc-software.sh
@@ -6,7 +6,7 @@ sudo apt install -y steam mesa-vulkan-drivers libglx-mesa0:i386 mesa-vulkan-driv
 wget -nc https://dl.winehq.org/wine-builds/winehq.key
 sudo apt-key add winehq.key
 
-echo "deb https://dl.winehq.org/wine-builds/debian/ bullseye main" >> /etc/apt/sources.list.d/wine.list
+echo "deb https://dl.winehq.org/wine-builds/debian/ bullseye main" | sudo tee /etc/apt/sources.list.d/lutris.list
 sudo apt update
 sudo apt install -y --install-recommends winehq-stable
 
