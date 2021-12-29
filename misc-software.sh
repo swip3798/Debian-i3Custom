1910a4cd50f5e104eb22e3658337df5eea2e76c3
Christian <christian.sw@hotmail.de>
2021-12-29 23:13:05 +0100

    Disclaimer

--- misc-software.sh
+++ misc-software.sh
@@ -1,3 +1,4 @@
+###### As an extra script since it takes A LOT of storage
 # Install steam
 sudo apt update
 sudo apt install -y steam mesa-vulkan-drivers libglx-mesa0:i386 mesa-vulkan-drivers:i386 libgl1-mesa-dri:i386
