7e754264aa522fc872b1a99fd66305b231a436b7
Christian <christian.sw@hotmail.de>
2022-01-04 16:41:20 +0100

    Fix install alacritty

--- install-alacritty.sh
+++ install-alacritty.sh
@@ -1,8 +1,9 @@
 git clone https://github.com/alacritty/alacritty.git
 cd alacritty
 
-curl --proto '=https' --tlsv1.2 -sSf https://sh.rustup.rs | sh
+curl --proto '=https' --tlsv1.2 -sSf https://sh.rustup.rs | sh -s -- -y
+source $HOME/.cargo/env
 
-sudo apt-get install -y cmake pkg-config libfreetype6-dev libfontconfig1-dev libxcb-xfixes0-dev libxkbcommon-dev python3
+sudo apt install -y cmake pkg-config libfreetype6-dev libfontconfig1-dev libxcb-xfixes0-dev libxkbcommon-dev python3
 cargo build --release
 cd ..
