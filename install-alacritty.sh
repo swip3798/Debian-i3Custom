d243210b8a22783d55eb909ebb237f10312f423b
Christian <christian.sw@hotmail.de>
2022-01-04 16:45:41 +0100

    Actually make an install

--- install-alacritty.sh
+++ install-alacritty.sh
@@ -6,4 +6,10 @@ source $HOME/.cargo/env
 
 sudo apt install -y cmake pkg-config libfreetype6-dev libfontconfig1-dev libxcb-xfixes0-dev libxkbcommon-dev python3
 cargo build --release
+
+sudo cp target/release/alacritty /usr/local/bin # or anywhere else in $PATH
+sudo cp extra/logo/alacritty-term.svg /usr/share/pixmaps/Alacritty.svg
+sudo desktop-file-install extra/linux/Alacritty.desktop
+sudo update-desktop-database
+
 cd ..
