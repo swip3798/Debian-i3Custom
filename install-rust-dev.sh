6e0b0dd09a1f34b1e67ba49e98f1cd7a306b744d
Christian Schweigel <christian.sw@hotmail.de>
2022-10-25 09:57:32 +0200

    Add lsd

--- install-rust-dev.sh
+++ install-rust-dev.sh
@@ -1,2 +1,8 @@
 curl --proto '=https' --tlsv1.2 -sSf https://sh.rustup.rs | sh -s -- -y
 source "$HOME/.cargo/env"
+echo This will install lsd CLI tool and add an command alias. If you want to skip that just type Ctrl+C
+read -p "Press enter to continue"
+
+cargo install lsd
+echo alias lsd="lsd -l" >> ~/.bashrc
+source "~/.bashrc"
