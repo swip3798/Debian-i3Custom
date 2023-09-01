5845c5e40ec18eb6939bd8e8661fe24bd04f9f2a
Christian Schweigel <33018263+swip3798@users.noreply.github.com>
2023-09-01 10:47:03 +0200

    Update install-firefox-stable.sh

--- install-firefox-stable.sh
+++ install-firefox-stable.sh
@@ -7,7 +7,7 @@ wget -O firefox.tar.bz2 "https://download.mozilla.org/?product=firefox-latest-ss
 sudo tar -jxvf firefox.tar.bz2 -C /opt
 rm firefox.tar.bz2
 
-sudo cp desktop-files/firefox-stable.desktop /usr/share/applications/firefox-stable.desktop-files
+sudo cp desktop-files/firefox-stable.desktop /usr/share/applications/firefox-stable.desktop
 sudo ln -s /opt/firefox/firefox /usr/local/bin/firefox 
 sudo update-alternatives --install /usr/bin/x-www-browser x-www-browser /opt/firefox/firefox 200 && sudo update-alternatives --set x-www-browser /opt/firefox/firefox  
 
