a811ac253150e89dd5ae16aa3aec0ae635e15eb7
Christian Schweigel <33018263+swip3798@users.noreply.github.com>
2023-09-03 13:18:13 +0200

    Update install-firefox-stable.sh

--- install-firefox-stable.sh
+++ install-firefox-stable.sh
@@ -3,6 +3,10 @@ read -p "Press Enter to continue"
 sudo rm -rf /opt/firefox
 sudo apt remove -y firefox-esr
 
+echo Install dependencies (if not installed already)
+read -p "Press Enter to continue"
+sudo apt install -y libdbus-glib-1-2 libevent-2.1-7 libnspr4 libnss3
+
 wget -O firefox.tar.bz2 "https://download.mozilla.org/?product=firefox-latest-ssl&os=linux64&lang=en-US"
 sudo tar -jxvf firefox.tar.bz2 -C /opt
 rm firefox.tar.bz2
